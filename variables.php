89c42bec0671bbfcdb18e195ed8da24ca12a3b7f
1t-pensan <ppatrik0319@gmail.com>
2025-09-04 10:46:29 +0200

    variables php

---
+++ variables.php
@@ -0,0 +1,13 @@
+<?php 
+    /*
+    szükséges programok: XAMPP (Appache MySQL),Git,GitHUB,repo,VSC
+    <?php ... ?>
+    Változók(angol kell lenni) 
+    */
+    $name = "patrik";
+    $age = 18;
+    $city ="Kiskunfélegyháza";
+    var_dump($name);
+    echo "<br>";
+    echo "Name: $name életkor: $age lakohely: $city"
+?> 
