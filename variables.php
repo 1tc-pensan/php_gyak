54028f85d8f2998d9bc92dbb368cad6a6f95e50d
1t-pensan <ppatrik0319@gmail.com>
2025-09-04 11:19:28 +0200

    varibles.php kész

--- variables.php
+++ variables.php
@@ -9,5 +9,30 @@
     $city ="Kiskunfélegyháza";
     var_dump($name);
     echo "<br>";
-    echo "Name: $name életkor: $age lakohely: $city"
+    echo "Name: $name életkor: $age lakohely: $city";
+    //Konstans
+    echo "<br>";
+    define("PI",3.14);
+    echo PI;
+    //adattipusok:string,int,float,bool,array
+    $message="lakatosarmadnoazifjabikmagyarszármazásúúriember";
+    echo "<br>Kiir: ","$message","értéket","<br>\n";
+    echo "<br>Kiir: $message értéket.<br>\n";
+    echo "<br>Kiir: {$message}darab értéket.<br>\n";
+
+    print "Kiir: ".$message."értéket"."<br>\n"
+
+    /*
+    git parancsok
+     git inti:helyi repo incializálás
+     git add . :fileok hozzad adása a staging areahoz
+     git commit -m "message" :
+     git remote add origin https://........... : hozzá rendeljük a helyit a távolihoz
+        git config --global user.email "email" 
+        git config --global user.name "name" 
+     git branch -M main:a branch főág megnevezése
+     git push -u origin main: hely vált a távoliba
+
+    HF:szedd le a saját mappát a htdocsbas
+    */
 ?> 
