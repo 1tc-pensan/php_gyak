02ea578f689949aca5f7d9efcee0f6afae01a859
1t-pensan <ppatrik0319@gmail.com>
2025-09-08 08:43:22 +0200

    késZ

---
+++ structures.php
@@ -0,0 +1,19 @@
+<?php 
+/*
+1.if else elseif
+2.switch
+3.Ciklusok for,while
+4.Ternary operator
+5.Tömbök (indexelt asszociat  v,tömbök tömbje)
+*/ 
+$number=8;
+echo "a {$number} ";
+if($number%2)
+{
+    echo "nem páros";
+}
+else
+{
+    echo "páros";
+}
+?>
