df00e5de48646b28ad38f7815836a05cfea68f3e
1t-pensan <ppatrik0319@gmail.com>
2025-09-08 09:23:41 +0200

    nem tudom mi ez

--- structures.php
+++ structures.php
@@ -16,4 +16,38 @@ else
 {
     echo "páros";
 }
+//1-10 ig szám
+for ($i=1; $i < 11; $i++) 
+{ 
+    echo "<br>$i";
+}
+//hozz létre egy indexelt tömböt 5 gyüm névvel.
+$fruits =["apple","apricot","banana","orange","plum",];
+$things=array("this","that");
+for ($i=0; $i <count($fruits) ; $i++) 
+{ 
+    echo "<br>$fruits[$i]";
+}
+//hozz létre user t. kor névvel
+$users=
+[
+    "Kiss Pista" =>20,
+    "Nagy István" =>32,
+    "Lakatos Kolompár" =>16
+];
+foreach ($users as $name => $age) 
+{
+    echo "<br>$name $age";
+}
+$students=
+[
+    ["name" => "Kovács Jözsi","age" => 21],
+    ["name" => "Nagy Pista","age" => 31],
+    ["name" => "Lakatos Armando","age" =>16]
+];
+echo "<br>";
+foreach ($students as $student) 
+{
+    echo "<br>$student[name] $student[age]";
+}
 ?>
